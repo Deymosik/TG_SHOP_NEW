ac80ad812518b3f65fc5f058cf30ff27b3702b65
Deymosik <86353985+Deymosik@users.noreply.github.com>
2025-07-21 12:53:21 +0600

    Delete frontend directory

--- frontend/src/config.js
+++
@@ -1,8 +0,0 @@
-// frontend/src/config.js
-
-// КОНФИГУРАЦИЯ ВАШЕГО МАГАЗИНА
-export const SHOP_CONFIG = {
-    // !!! ЗАМЕНИТЕ 'YourManagerUsername' НА РЕАЛЬНЫЙ ЮЗЕРНЕЙМ МЕНЕДЖЕРА В TELEGRAM !!!
-    // Указывать без символа @. Например: 'ivan_petrov_shop'
-    MANAGER_USERNAME: 'Deymosik'
-};
